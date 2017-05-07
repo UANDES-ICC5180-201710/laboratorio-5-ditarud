50a90753d63bf1386a1afc8f662159edfe30b5f4
ditarud <ditarud@miuandes.cl>
2017-05-07 20:19:28 -0300

    Lab5

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :grades
   resources :assignments
   resources :enrollments
-  resources :courses
+  resources :courses do
+    get "students", to: "courses#students" , on: :member
+  end
   resources :people
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
